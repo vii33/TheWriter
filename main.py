5a0d8b2f49777c169dd48146536048b3a47fca41
derveit <github.20.losvochos@spamgourmet.com>
2020-03-14 23:50:04 +0100

    extracted consts

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import *
 import theUI
 
+# Individual Settings
+SPEECH_KEY = "KEY"
+SERVICE_REGION = "westeurope"
 
 if __name__ == '__main__':
     # Initialize Window
     root = Tk()
-    win = theUI.Window(master=root, speech_key="KEY", 
-                service_region="westeurope")
+    win = theUI.Window(master=root, speech_key=SPEECH_KEY, 
+                service_region=SERVICE_REGION)
 
