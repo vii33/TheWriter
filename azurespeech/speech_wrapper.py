b0ebc08ab405d3784a88ac4dd1bc1a7fe81a6575
derveit <github.20.losvochos@spamgourmet.com>
2021-01-05 18:36:27 +0100

    Renamed package

--- azurespeech/speech_wrapper.py
+++ azurespeech/speech_wrapper.py
@@ -1,51 +1,7 @@
 import azure.cognitiveservices.speech as speechsdk
 from datetime import datetime
-#import time    # used for pretty cmdline output
-#from _thread import start_new_thread
-#from os import system   #used for pretty cmdline output
 
-
-# Manual: https://github.com/Azure-Samples/cognitive-services-speech-sdk/blob/67bd7c502d5a859fd981227ce1cf60f51cd5e112/samples/python/console/speech_sample.py#L196
-
-#AUTO DETECTION OF LANG (not avail for python currently)
-# in sharp doesnt work very well on live changing languages (02/2020)
-#https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-automatic-language-detection?pivots=programming-language-csharp
-
-
-def text_splitter(data: str, split_num: int, result: list):
-    tmp = data.split(maxsplit=split_num)
-    #print("tmp: {}".format(tmp))
-    
-    if len(tmp) > split_num:
-        # Store triplets
-        result.append(tmp[0:split_num])
-        # recursion
-        #print("kkk")
-        text_splitter(tmp[split_num], split_num, result) # potential bug if more elements then splitnum
-        #print("recursion.end1 -")
-        return result
-    else:
-        #less then splitnum elements: append rest
-        #print("lll")
-        result.append(tmp)
-        #print("recursion.end2 -")
-        return result
-
-def text_printer (dataarray: str):
-    system('cls') 
-    for i in range(len(dataarray)):
-        print(" ".join(dataarray[i]))
-
-def pretty_print(evt_text: str):
-    result = []
-    result = text_splitter(evt_text, 5, result)
-    #print("result: {}".format(result))
-    text_printer(result)
-
-
-
-
-class  Speech_Rec:
+class  Speech_Wrapper:
     def __init__(self, speech_key: str, service_region: str, 
             recognition_language: str, callbackUI):
         
@@ -70,10 +26,20 @@ class  Speech_Rec:
         self.speech_recognizer.session_stopped.connect(self.stop_cb)
         self.speech_recognizer.canceled.connect(self.stop_cb)
 
+        # Improve recognition accuracy with custom lists 
+        self.update_grammar_list(self.speech_recognizer)
+    
+    def update_grammar_list(self, speech_recognizer):
+        """
+        Improves recognition accuracy with custom lists. Currently does not work correctly (Azure Bug)
+        """
+        phrase_list_grammar = speechsdk.PhraseListGrammar.from_recognizer(speech_recognizer)
+        phrase_list_grammar.addPhrase("Hermo")
+
 
     def cb_recognized(self, evt):
         """callback for finshed recoginzed sentences. mostly gets called after a speaking pause"""
-        print("R: {}".format(evt.result.text))
+        #print("R: {}".format(evt.result.text))
         
         key = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
         self.conversation[key] = evt.result.text
@@ -82,8 +48,7 @@ class  Speech_Rec:
     def cb_recogonizing(self, evt):
         """callback for recoginzed sentences. Gets updated frequently but words change sometimes (due to /
         sentence context info"""
-        print("T: {}".format(evt.result.text))
-        #pretty_print(evt.result.text)
+        #print("T: {}".format(evt.result.text))
                 
         self.callback_UI.mytext_live.set(evt.result.text)
         
@@ -99,7 +64,6 @@ class  Speech_Rec:
     def stop_cb(self, evt):
         """callback that signals to stop continuous recognition upon receiving an event `evt`"""
         print('CLOSING on {}'.format(evt))
-        #nonlocal done   # from the MS example. doesn't work
         done = True
 
 
