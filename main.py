a79f254a7b5cbedab32f86602e1f43bb09a8c75d
derveit <github.20.losvochos@spamgourmet.com>
2021-01-05 18:36:03 +0100

    Introduced cmd line parameters

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import sys, getopt
 from tkinter import *
 import theUI
 
-# Individual Settings
-SPEECH_KEY = "KEY"
-SERVICE_REGION = "westeurope"
 
-if __name__ == '__main__':
+def main(argv):
+
+    SPEECH_KEY = "KEY"
+    SERVICE_REGION = "westeurope"
+
+    try:
+        opts, args = getopt.getopt(argv,"k:r:")
+    except getopt.GetoptError:
+        print ('main.py -k <AZURE_SPEECH_KEY> -r <AZURE_SERVICE_REGION>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('main.py -k <AZURE_SPEECH_KEY> -r <AZURE_SERVICE_REGION>')
+            sys.exit()
+        elif opt in ("-k", "--k"):
+            SPEECH_KEY = arg
+        elif opt in ("-r", "--r"):
+            SERVICE_REGION = arg
+        else:
+            assert False, "unhandled option"
+
     # Initialize Window
     root = Tk()
     win = theUI.Window(master=root, speech_key=SPEECH_KEY, 
                 service_region=SERVICE_REGION)
 
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
